91a882a5c878a14b5bf84b594eeea2d04f734332
PearsonCodes <alexpearson003@gmail.com>
2026-06-29 13:01:40 +0100

    Create tip_calculator.py

---
+++ Day02/tip_calculator.py
@@ -0,0 +1,12 @@
+# DAY 2 Project - Tipping Calculator
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+tip_amount = tip / 100 * bill
+
+total_bill_pp = (bill + tip_amount) / people
+
+print(f"Each person should pay ${total_bill_pp}")
